fe35f0587ce26effe9161bf253890733dbb9af94
nosenkoan <nosenkoan@answer-42.ru>
2024-06-13 14:37:55 +0300

    include port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, jsonify, request
 import requests
 
@@ -25,4 +27,4 @@ def get_tournament_results(tournament_id):
     return jsonify( data )
 
 if __name__ == '__main__':
-    app.run(debug=False, host = '0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
